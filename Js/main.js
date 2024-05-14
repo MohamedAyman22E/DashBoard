8eecacd09b018df2c256520d4f49a401d07be67b
Mohamed Ayman <143669883+MohamedAyman22E@users.noreply.github.com>
2024-05-14 04:20:55 +0300

    changes

--- Js/main.js
+++ Js/main.js
@@ -7,3 +7,27 @@ menu.addEventListener("click", () => {
 close.addEventListener("click", () => {
   navBar.classList.remove("active");
 });
+// start toggle
+let allSpan = document.querySelectorAll(".right_container .top .toggle span");
+let sun = document.querySelector(".right_container .top  .sun");
+let moon = document.querySelector(".right_container .top .moon");
+
+allSpan.forEach(e => {
+  e.addEventListener("click", () => {
+    allSpan.forEach(x => {
+      x.classList.remove("active");
+      document.body.classList = "";
+      localStorage.setItem("Background", "");
+    });
+    e.classList.add("active");
+    if (sun.classList.contains("active")) {
+      document.querySelector("body").classList.add("sun");
+      document.body.classList.add("sun");
+      localStorage.setItem("Background", "sun");
+    } else {
+      document.body.classList.add("moon");
+      localStorage.setItem("Background", "moon");
+    }
+  });
+});
+document.querySelector("body").classList = localStorage.getItem("Background");
